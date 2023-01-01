eefc4bed4916af3fa1946909b81dc1764be81f5b
kbstein <kbsteinicke@gmail.com>
2023-01-01 10:09:31 -0700

    Initial Commit

---
+++ SmartSimpleCalories/SmartSimpleCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartSimpleCaloriesApp.swift
+//  SmartSimpleCalories
+//
+//  Created by Kyle Steinicke on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartSimpleCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
