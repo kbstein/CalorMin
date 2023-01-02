0334cf54e4d0650455bc6d59e0f21279f3d28eb3
kbstein <kbsteinicke@gmail.com>
2023-01-02 10:55:48 -0700

    Changed entering calories from sheet to Zstack

--- SmartSimpleCalories/HomeView.swift
+++ SmartSimpleCalories/HomeView.swift
@@ -22,89 +22,95 @@ struct HomeView: View {
 
 
     var body: some View {
-        VStack {
-            Button(action:  {
-                self.showDayHistoryView = true
-            }) {
-                Text("\(numberOfCalories)")
-                    .foregroundColor(.black)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .frame(width: 250, height: 75)
-                
-                    .background(Color.gray)
-                    .cornerRadius(/*@START_MENU_TOKEN@*/18.0/*@END_MENU_TOKEN@*/)
-            }.sheet(isPresented: $showDayHistoryView) {
-                DayHistoryView()
-            }
-            Text(textBelowCalorieCount)
-                .font(.title2)
-                .fontWeight(.semibold)
-            Spacer()
-            Button(action: {
-                self.focusedField = .field
-                self.showNumberKeyboard = true
-            }) {
-                Image(systemName: "plus")
-                    .frame(width: 50, height: 50)
+        ZStack {
+            VStack {
+                Button(action:  {
+                    self.showDayHistoryView = true
+                }) {
+                    Text("\(numberOfCalories)")
+                        .foregroundColor(.black)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .frame(width: 250, height: 75)
+                    
+                        .background(Color.gray)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/18.0/*@END_MENU_TOKEN@*/)
+                }.sheet(isPresented: $showDayHistoryView) {
+                    DayHistoryView()
+                }
+                Text(textBelowCalorieCount)
                     .font(.title2)
                     .fontWeight(.semibold)
-                    .foregroundColor(Color.black)
+                Spacer()
+                Button(action: {
+                    self.showNumberKeyboard = true
+                }) {
+                    Image(systemName: "plus")
+                        .frame(width: 50, height: 50)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.black)
+                }
+                .background(Color.gray)
+                .cornerRadius(18.0)
+                .padding()
             }
-            .background(Color.gray)
-            .cornerRadius(18.0)
-            .padding()
-            .sheet(isPresented: $showNumberKeyboard) {
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.brown)
+            if (showNumberKeyboard) {
+                VisualEffectView(effect: UIBlurEffect(style: .dark)).edgesIgnoringSafeArea(.all)
                 VStack {
-                    VStack {
-                        HStack {
-                            Button(action: {
-                                self.showNumberKeyboard = false
-                                enteredCalories = ""
-                            }) {
-                                Image(systemName: "x.square")
-                                    .frame(width: 50, height: 50)
-                                    .font(.title)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(Color.black)
-                                    .background(Color.red)
-                                    .cornerRadius(18.0)
-                            }
-                            TextField("0", text: self.$enteredCalories, onCommit: {
-                                self.numberOfCalories += Int(self.enteredCalories) ?? 0
-                            })
-                            .focused($focusedField, equals: .field)
-                            .keyboardType(.numberPad)
-                            .font(.title)
-                            .fontWeight(.semibold)
-                            .background(Color.green)
-                            Button(action: {
-                                numberOfCalories -= Int(enteredCalories) ?? 0
-                                self.showNumberKeyboard = false
-                                enteredCalories = ""
-                            }) {
-                                Image(systemName: "checkmark.square")
-                                    .frame(width: 50, height: 50)
-                                    .font(.title)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(Color.black)
-                                    .background(Color.green)
-                                    .cornerRadius(18.0)
-                            }
+                    HStack {
+                        Button(action: {
+                            self.showNumberKeyboard = false
+                            enteredCalories = ""
+                        }) {
+                            Image(systemName: "x.square")
+                                .frame(width: 50, height: 50)
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color.black)
+                                .background(Color.red)
+                                .cornerRadius(18.0)
+                        }
+                        TextField("0", text: self.$enteredCalories, onCommit: {
+                            self.numberOfCalories += Int(self.enteredCalories) ?? 0
+                        })
+                        .onAppear {
+                            self.focusedField = .field
+                        }
+                        .focused($focusedField, equals: .field)
+                        .keyboardType(.numberPad)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .background(Color.gray)
+                        Button(action: {
+                            numberOfCalories -= Int(enteredCalories) ?? 0
+                            self.showNumberKeyboard = false
+                            enteredCalories = ""
+                        }) {
+                            Image(systemName: "checkmark.square")
+                                .frame(width: 50, height: 50)
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color.black)
+                                .background(Color.green)
+                                .cornerRadius(18.0)
                         }
                     }
-                    .frame(width: 200, height: 200)
-                    .background(Color.blue)
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.black)
+                .frame(width: 275, height: 200)
+                .background(Color.white)
+                .cornerRadius(18.0)
             }
-            .ignoresSafeArea(.keyboard, edges: .bottom)
-                
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.brown)
     }
+}
+
+struct VisualEffectView: UIViewRepresentable {
+    var effect: UIVisualEffect?
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
 }
 
 struct DayHistoryView: View {
