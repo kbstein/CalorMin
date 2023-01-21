bd4533035bd31f54bec74ecae08abb577aa30cbf
kbstein <kbsteinicke@gmail.com>
2023-01-21 12:03:30 -0700

    - Implemented day of the week shifter and display - Started bar graph view

--- SmartSimpleCalories/Views/GraphView.swift
+++ SmartSimpleCalories/Views/GraphView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 
 struct GraphView: View {
-
+    var deviceHeight: CGFloat {
+        UIScreen.main.bounds.height
+    }
     var dayOfWeek: Int
-    let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let daysOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-    
     var body: some View {
-        VStack {
-            Text("\(daysOfTheWeek[dayOfWeek - 1])")
+        ZStack {
+            VStack {
+                let daysToDisplay = shiftDaysOfTheWeek(currentDay: dayOfWeek)
+                Text("Daily Calories")
+                    .font(.title)
+                Text("Todays Count")
+                    .font(.title2)
+                    .padding()
+                Spacer()
+                HStack {
+                    ForEach(0..<7) { day in
+                        VStack {
+                            Spacer()
+                            Rectangle()
+                            .fill(Color.red)
+                            .frame(width: 20, height: deviceHeight * 0.4)
+                            Text("\(daysToDisplay[day])")
+                                .frame(width: 40)
+                        }
+                    }
+                }
+                Spacer().frame(height: 10)
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color("BackgroundGray"))
+    }
+    
+    func shiftDaysOfTheWeek(currentDay: Int) -> [String] {
+        var shiftedDaysOfTheWeek = daysOfTheWeek
+        for _ in 1..<currentDay + 1 {
+            shiftedDaysOfTheWeek.append(shiftedDaysOfTheWeek.remove(at: 0))
+        }
+        return shiftedDaysOfTheWeek
     }
 }
 
@@ -32,6 +62,6 @@ struct GraphView: View {
 
 struct GraphView_Previews: PreviewProvider {
     static var previews: some View {
-        GraphView(dayOfWeek: 1)
+        GraphView(dayOfWeek: 4)
     }
 }
